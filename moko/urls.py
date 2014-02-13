20aaea8a73ac8319521e34afefaee3a3b5cc099a
Kwabena Aning <kwabena.aning@gmail.com>
2014-02-13 17:06:11 +0000

    Fix for urls

--- moko/urls.py
+++ moko/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        # Examples:
                        url(r'^$', 'moko.views.home', name='home'),
-                       (r'^photos/', include('photos.urls')),
+                       (r'^photos', include('photos.urls')),
+                       (r'^photos/', include('photos.urls', namespace='photos')),
                        (r'^admin/', include(admin.site.urls)),
 )
--- photos/urls.py
+++ photos/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns,  url
+from django.conf.urls import patterns, url
 from photos.views import PhotosTemplate, PhotoViewTemplate
 
 urlpatterns = patterns('',
                        url(r'^/$', PhotosTemplate.as_view()),
-                       url(r'^photos', PhotosTemplate.as_view()),
                        url(r'^view/(?P<image_id>\w+)/$', PhotoViewTemplate.as_view()),
                        url(r'^directory/(?P<image_id>\w+)/$', 'photos.views.list'),
 )
