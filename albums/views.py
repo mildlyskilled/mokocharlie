4e796e89bd647def8e13ad6312677074a4b233aa
Kwabena Aning <kwabena.aning@gmail.com>
2014-02-14 15:21:39 +0000

    Pagination for albums and images within single album views

--- albums/views.py
+++ albums/views.py
@@ -1,21 +1,52 @@
 from django.views.generic.base import TemplateView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from albums.models import *
+from photos.models import *
 
 
 class AlbumTemplate(TemplateView):
     template_name = "albums/index.html"
+    default_limit = 20
+    default_page = 1
 
     def get_context_data(self, **kwargs):
-        albums = Album.objects.all()
+
+        albums = Album.objects.all().order_by('-date_added')
+        limit = self.request.GET.get('limit', self.default_limit)
+        _page = self.request.GET.get('page', self.default_page)
+        p = Paginator(albums, limit)
+        try:
+            album_list = p.page(_page)
+        except PageNotAnInteger:
+            album_list = p.page(self.default_page)
+        except EmptyPage:
+            album_list = p.page(p.num_pages)
+
         context = super(AlbumTemplate, self).get_context_data()
-        context["albums"] = albums
+        context["albums"] = album_list
         return context
 
 
 class AlbumViewTemplate(TemplateView):
     template_name = "albums/view.html"
+    default_limit = 10
+    default_page = 1
 
     def get_context_data(self, **kwargs):
+        album_id = self.kwargs.get('album_id')
+        limit = self.request.GET.get('limit', self.default_limit)
+        _page = self.request.GET.get('page', self.default_page)
+        album = Album.objects.get(album_id=album_id)
+
+        p = Paginator(Photo.objects.filter(image_album=album_id), limit)
+        try:
+            image_list = p.page(_page)
+        except PageNotAnInteger:
+            image_list = p.page(self.default_page)
+        except EmptyPage:
+            image_list = p.page(p.num_pages)
+
         context = super(AlbumViewTemplate, self).get_context_data()
-        context["album"] = Album.objects.get(album_id=self.kwargs.get('album_id'))
+        context["album_images"] = image_list
+        context["album"] = album
         return context
