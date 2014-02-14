d00487a5216e70f75812daa9d6fe767eca0254a8
Kwabena Aning <kwabena.aning@gmail.com>
2014-02-14 16:36:20 +0000

    Added paginator partial

--- albums/views.py
+++ albums/views.py
@@ -12,9 +12,9 @@ class AlbumTemplate(TemplateView):
     def get_context_data(self, **kwargs):
 
         albums = Album.objects.all().order_by('-date_added')
-        limit = self.request.GET.get('limit', self.default_limit)
+        _limit = self.request.GET.get('limit', self.default_limit)
         _page = self.request.GET.get('page', self.default_page)
-        p = Paginator(albums, limit)
+        p = Paginator(albums, _limit)
         try:
             album_list = p.page(_page)
         except PageNotAnInteger:
@@ -24,6 +24,7 @@ class AlbumTemplate(TemplateView):
 
         context = super(AlbumTemplate, self).get_context_data()
         context["albums"] = album_list
+        context["paginator_object"] = album_list
         return context
 
 
@@ -34,11 +35,11 @@ class AlbumViewTemplate(TemplateView):
 
     def get_context_data(self, **kwargs):
         album_id = self.kwargs.get('album_id')
-        limit = self.request.GET.get('limit', self.default_limit)
+        _limit = self.request.GET.get('limit', self.default_limit)
         _page = self.request.GET.get('page', self.default_page)
         album = Album.objects.get(album_id=album_id)
 
-        p = Paginator(Photo.objects.filter(image_album=album_id), limit)
+        p = Paginator(Photo.objects.filter(image_album=album_id), _limit)
         try:
             image_list = p.page(_page)
         except PageNotAnInteger:
@@ -48,5 +49,6 @@ class AlbumViewTemplate(TemplateView):
 
         context = super(AlbumViewTemplate, self).get_context_data()
         context["album_images"] = image_list
+        context["paginator_object"] = image_list
         context["album"] = album
         return context
--- photos/views.py
+++ photos/views.py
@@ -1,14 +1,29 @@
 from django.views.generic.base import TemplateView
 from photos.models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class PhotosTemplate(TemplateView):
     template_name = "photos/index.html"
+    default_limit = 20
+    default_page = 1
+
 
     def get_context_data(self, **kwargs):
         images = Photo.objects.all()
+        _limit = self.request.GET.get('limit', self.default_limit)
+        _page = self.request.GET.get('page', self.default_page)
+        p = Paginator(images, _limit)
+        try:
+            image_list = p.page(_page)
+        except PageNotAnInteger:
+            image_list = p.page(self.default_page)
+        except EmptyPage:
+            image_list = p.page(p.num_pages)
+
         context = super(PhotosTemplate, self).get_context_data()
-        context["images"] = images
+        context["images"] = image_list
+        context["paginator_object"] = image_list
         return context
 
 
